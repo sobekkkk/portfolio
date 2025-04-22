2b7b5f5485a8e92df5734b4922bf91cea1e2081a
sobekkkk <0xsobek@proton.me>
2025-04-22 09:00:04 +0200

    test deploy

--- astro.config.ts
+++ astro.config.ts
@@ -10,43 +10,43 @@ import { spectreDark } from './src/ec-theme';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://spectre.louisescher.dev',
-  output: 'static',
-  integrations: [
-    expressiveCode({
-      themes: [spectreDark],
-    }),
-    mdx(),
-    sitemap(),
-    spectre({
-      name: 'Spectre',
-      openGraph: {
-        home: {
-          title: 'Spectre',
-          description: 'A minimalistic theme for Astro.'
-        },
-        blog: {
-          title: 'Blog',
-          description: 'News and guides for Spectre.'
-        },
-        projects: {
-          title: 'Projects'
-        }
-      },
-      giscus: {
-        repository: 'louisescher/spectre',
-        repositoryId: 'R_kgDONjm3ig',
-        category: 'General',
-        categoryId: 'DIC_kwDONjm3is4ClmBF',
-        mapping: 'pathname',
-        strict: true,
-        reactionsEnabled: true,
-        emitMetadata: false,
-        lang: 'en',
-      }
+    site: 'https://sobekkkk.github.io/portfolio',
+    output: 'static',
+    integrations: [
+        expressiveCode({
+            themes: [spectreDark]
+        }),
+        mdx(),
+        sitemap(),
+        spectre({
+            name: 'Spectre',
+            openGraph: {
+                home: {
+                    title: 'Spectre',
+                    description: 'A minimalistic theme for Astro.'
+                },
+                blog: {
+                    title: 'Blog',
+                    description: 'News and guides for Spectre.'
+                },
+                projects: {
+                    title: 'Projects'
+                }
+            },
+            giscus: {
+                repository: 'louisescher/spectre',
+                repositoryId: 'R_kgDONjm3ig',
+                category: 'General',
+                categoryId: 'DIC_kwDONjm3is4ClmBF',
+                mapping: 'pathname',
+                strict: true,
+                reactionsEnabled: true,
+                emitMetadata: false,
+                lang: 'en'
+            }
+        })
+    ],
+    adapter: node({
+        mode: 'standalone'
     })
-  ],
-  adapter: node({
-    mode: 'standalone'
-  })
 });
